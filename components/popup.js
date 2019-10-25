d1b731e27f5cca2ab1b4d8ab40767894e0554842
Ruochen Lyu <kshift.lv@gmail.com>
2019-10-26 01:58:43 +0800

    chage: update classifo.js & style

--- components/popup.js
+++ components/popup.js
@@ -1,39 +1,41 @@
-import React from "react";
-import ClassInfo from "../classinfo";
-import style from "../styles/popup.scss";
+import React from 'react'
+import ClassInfo from '../classinfo'
+import style from '../styles/popup.scss'
 
 class Popup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.pressESC = this.pressESC.bind(this);
+  constructor (props) {
+    super(props)
+    this.pressESC = this.pressESC.bind(this)
   }
 
-  pressESC(event) {
+  pressESC (event) {
     if (event.keyCode === 27) {
-      this.props.onMaskClick();
+      this.props.onMaskClick()
     }
   }
 
-  componentDidMount() {
-    document.addEventListener("keydown", this.pressESC, false);
+  componentDidMount () {
+    document.addEventListener('keydown', this.pressESC, false)
   }
 
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.pressESC, false);
+  componentWillUnmount () {
+    document.removeEventListener('keydown', this.pressESC, false)
   }
 
-  render() {
-    if (!this.props.classId) return null;
-    const data = ClassInfo[this.props.classId.toString()];
-    if (!data) return null;
+  render () {
+    if (!this.props.classId) return null
+    const data = ClassInfo[this.props.classId.toString()]
+    if (!data) return null
 
     const video = data.video ? (
       <iframe
-        frameBorder="0"
-        src={`//v.qq.com/txp/iframe/player.html?vid=${data.video}&autoplay=true&mini=true&disableplugin=IframeBottomOpenClientBar,IframeEndRecommend,IframePauseRecommend,IframeRelateRecommend`}
-        allowFullScreen={true}
+        frameBorder='0'
+        src={`//v.qq.com/txp/iframe/player.html?vid=${
+          data.video
+        }&autoplay=true&mini=true&disableplugin=IframeBottomOpenClientBar,IframeEndRecommend,IframePauseRecommend,IframeRelateRecommend`}
+        allowFullScreen
       />
-    ) : null;
+    ) : null
 
     return (
       <div className={style.popup}>
@@ -43,15 +45,19 @@ class Popup extends React.Component {
           <div className={style.content}>
             <h2>{data.className}</h2>
             <p dangerouslySetInnerHTML={{ __html: data.classIntroduce }} />
-            <span className={style.title}>训练效果</span>
+            {data.trainingEffect && (
+              <span className={style.title}>训练效果</span>
+            )}
             <p dangerouslySetInnerHTML={{ __html: data.trainingEffect }} />
-            <span className={style.title}>适合人群</span>
+            {data.suitablePeople && (
+              <span className={style.title}>适合人群</span>
+            )}
             <p dangerouslySetInnerHTML={{ __html: data.suitablePeople }} />
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Popup;
+export default Popup
