b7250583f23a8aea4657a81759a7517a1b09e3ec
Ruochen Lyu <kshift.lv@gmail.com>
2019-12-02 11:54:06 +0800

    change: leancloud

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import fetch from "isomorphic-unfetch";
 import ReactGA from "react-ga";
+import AV from "leancloud-storage";
 
 import "../styles/reset.scss";
 import "../styles/global.scss";
@@ -33,33 +33,45 @@ class Index extends React.Component {
   componentDidMount() {
     ReactGA.initialize("UA-150431358-1");
     ReactGA.pageview(location.pathname);
+    AV.init({
+      appId: "omStNgknnXtGwJ5mV6wyIthz-gzGzoHsz",
+      appKey: "x6pStM61pw2sjQfUWDJF7Yz6",
+      serverURLs: "https://api-super.owcat.com"
+    });
+
     const currentCity = localStorage.getItem("currentCity") || "北京市";
     this.fetch(currentCity);
   }
 
   async fetch(currentCity) {
     this.setState({ loading: true });
-    const result = await fetch(
-      `//raw.githubusercontent.com/RuochenLyu/ruochenlyu.github.io/master/supermonkey/${currentCity}.json`
-    );
-    const json = await result.json();
+    console.log(currentCity);
 
-    let boxId = localStorage.getItem("boxId") * 1;
-    const { cityMap } = json.data.main.boxArea;
-    const { boxList } = cityMap[currentCity][0];
+    new AV.Query("Course")
+      .equalTo("city", currentCity)
+      .find()
+      .then(data => {
+        if (data.length === 0) return;
+        data = data[0].attributes.data;
+        console.log(data);
 
-    if (boxList.every(item => item.boxId !== boxId)) {
-      boxId = boxList[0].boxId;
-    }
+        let boxId = localStorage.getItem("boxId") * 1;
+        const { cityMap } = data.main.boxArea;
+        const { boxList } = cityMap[currentCity][0];
 
-    this.setState({
-      loading: false,
-      data: json.data,
-      boxId,
-      currentCity,
-    });
-    localStorage.setItem("boxId", boxId);
-    localStorage.setItem("currentCity", currentCity);
+        if (boxList.every(item => item.boxId !== boxId)) {
+          boxId = boxList[0].boxId;
+        }
+
+        this.setState({
+          loading: false,
+          data,
+          boxId,
+          currentCity
+        });
+        localStorage.setItem("boxId", boxId);
+        localStorage.setItem("currentCity", currentCity);
+      });
   }
 
   render() {
